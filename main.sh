cddab58b2eae0d3c354b175f9c960f7d16a01160
plasmolysis <61166256+plasmolysis369@users.noreply.github.com>
2022-06-27 12:45:32 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -26,7 +26,8 @@ git clone --depth 1 https://github.com/fw876/helloworld
 git clone -b lede https://github.com/pymumu/luci-app-smartdns
 git clone https://github.com/KFERMercer/luci-app-tcpdump
 git clone https://github.com/plasmolysis369/mypackages
-git clone https://github.com/plasmolysis369/luci-app-store
+git clone https://github.com/koshev-msk/luci-app-smstools3
+git clone https://github.com/linkease/istore
 
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 svn co https://github.com/Ysurac/openmptcprouter-feeds/trunk/luci-app-iperf
