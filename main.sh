1178f207306efece5e7603a1b86c80947b3b7231
plasmolysis <61166256+plasmolysis369@users.noreply.github.com>
2022-10-09 18:34:39 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -14,7 +14,6 @@ git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-vssr
 git clone --depth 1 https://github.com/jerrykuku/luci-app-ttnode
 git clone --depth 1 https://github.com/sirpdboy/luci-app-advanced
-git clone --depth 1 https://github.com/jerrykuku/luci-app-jd-dailybonus
 git clone --depth 1 https://github.com/destan19/OpenAppFilter && mvdir OpenAppFilter
 git clone --depth 1 https://github.com/sensec/luci-app-udp2raw
 git clone --depth 1 https://github.com/LGA1150/openwrt-sysuh3c && mvdir openwrt-sysuh3c
@@ -26,8 +25,6 @@ git clone --depth 1 https://github.com/fw876/helloworld
 git clone -b lede https://github.com/pymumu/luci-app-smartdns
 git clone https://github.com/KFERMercer/luci-app-tcpdump
 git clone https://github.com/plasmolysis369/mypackages
-git clone https://github.com/koshev-msk/luci-app-smstools3
-git clone https://github.com/linkease/istore
 git clone https://github.com/xiaorouji/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall2
 
@@ -36,9 +33,9 @@ svn co https://github.com/Ysurac/openmptcprouter-feeds/trunk/luci-app-iperf
 svn co https://github.com/kenzok8/litte/trunk/luci-theme-atmaterial_new
 svn co https://github.com/kenzok8/litte/trunk/luci-theme-mcat
 svn co https://github.com/kenzok8/litte/trunk/luci-theme-tomato
-svn co https://github.com/linkease/nas-packages/trunk/network/services && mvdir services
-svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-netspeedtest
-svn co https://github.com/linkease/nas-packages-luci/trunk/luci && mvdir luci
+svn co https://github.com/linkease/nas-packages/trunk/network/services/ddnsto && mvdir ddnsto
+#svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-netspeedtest
+svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto && mvdir luci-app-ddnsto
 svn co https://github.com/sbilly/netmaker-openwrt/trunk/netmaker
 
 sed -i "s/mosdns-neo/mosdns/g" openwrt-mos/luci-app-mosdns/Makefile
